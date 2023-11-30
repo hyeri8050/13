b1f0091ac08a729e2751c01954eb5ec5c8c342f4
Hyeri Park <phr66@naver.com>
2023-11-30 15:34:15 +0900

    02

--- main.c
+++ main.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
 
-struct student
-{
-  int ID;
-  char name[100];
-  float score;
+struct point{
+  int x;
+  int y;
 };
 
 int main(int argc, char *argv[])
 {
-  //instance ¼±¾ð 
-  struct student s1 = {2111825, "HYERI", 4.3};
+  struct point p1, p2;
+  int xdiff, ydiff;
+  double dist;
   
-  s1.ID = 123456;
-  s1.name[0] = 'c';
-  s1.score = 2.5;
+  printf("input p1 coordinate (x y) : ");
+  scanf("%d %d", &p1.x, &p1.y);
   
-  printf("ID : %i, name : %s, score : %f\n", s1.ID, s1.name, s1.score);
-   
-  strcpy(s1.name, "Bongjun");
-  printf("name2: %s\n");
+  printf("input p2 coordinate (x y) : ");
+  scanf("%d %d", &p2.x, &p2.y);
+  
+  //distance
+  xdiff = p2.x - p1.x;
+  ydiff = p2.y - p1.y;
+  dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+  printf("distance : %f\n", dist);
   
   system("PAUSE");	
   return 0;
